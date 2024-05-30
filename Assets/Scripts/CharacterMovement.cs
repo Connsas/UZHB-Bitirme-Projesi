ad1ac977eb3cc459d0023ba00f9d43d308576a73
İsmail Öner <ismailoner1403@gmail.com>
2024-05-30 12:11:13 +0300

    Proje oluşturuldu

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    private CharacterController characterController;
+
+    [Header("Movement")]
+    [SerializeField] private float _speed = 4f, _Xspeed = 6f, _gravity = -9.8f, _jump = 1f;
+
+    [Header("Ground Check")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundDistance = 0.3f;
+    [SerializeField] private LayerMask groundMask;
+
+    [Header("Gravity and Direction")]
+    Vector3 _velocity;
+    private bool isGrounded;
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && _velocity.y < 0)
+        {
+            _velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        characterController.Move(move * _speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            characterController.Move(move * _Xspeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.Space) && isGrounded)
+        {
+            _velocity.y = Mathf.Sqrt(_jump * -2f * _gravity);
+        }
+
+        _velocity.y += _gravity * Time.deltaTime;
+
+        characterController.Move(_velocity * Time.deltaTime);
+    }
+}
